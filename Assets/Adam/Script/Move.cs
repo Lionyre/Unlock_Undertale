283cb79b3b5c5adfbd48fab008a85852de4ff246
AdamLepron <95347132+AdamLepron@users.noreply.github.com>
2022-02-04 17:03:07 +0100

    GamePlay Principal Ok

---
+++ Assets/Adam/Script/Move.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Move : MonoBehaviour
+{
+    [SerializeField] Transform _player = null;
+    [SerializeField] Slider _clickCounter;
+    public float PlayerSpeed;
+
+    public float clickPerSecond;
+
+    public float _timer;
+
+    private void Start()
+    {
+        _clickCounter.maxValue = _clickCounter.maxValue * PlayerSpeed;
+        _clickCounter.value = .1f;
+    }
+    private void Update()
+    {
+        _timer += Time.fixedDeltaTime;
+
+        _clickCounter.value = clickPerSecond/ _timer;
+
+        _player.position += new Vector3(_clickCounter.value / 10, 0, 0);
+    }
+
+    public void MovePlayer()
+    {
+        
+
+        clickPerSecond += PlayerSpeed;
+
+    }
+}
