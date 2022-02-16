ba19616dd2b2b2271d07d8dcc50af6ded272ee7f
Stephwgr <stephwgr59@gmail.com>
2022-02-16 13:07:30 +0100

    Commit resolution bug sprite

---
+++ Assets/Script/GameManagerSteph.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerSteph : MonoBehaviour
+{
+    public int _counter = 0;
+    public int _tileNumber = 0;
+
+    public GameObject _tileParent;
+
+    private void Update()
+    {
+        if (_counter == _tileNumber)
+        {
+            Debug.Log("Victoire");
+        }
+
+    }
+
+    private void Start()
+    {
+        _tileNumber = _tileParent.GetComponentsInChildren<Collider2D>().Length;
+
+    }
+}
