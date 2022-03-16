7b2b093722c775d7a52c889ac00da0dd031f2057
AdamLepron <95347132+AdamLepron@users.noreply.github.com>
2022-03-16 10:11:50 +0100

    commit fixe machine snail

--- Assets/Adam/Script/AdamMove.cs
+++ Assets/Adam/Script/AdamMove.cs
@@ -18,15 +18,21 @@ public class AdamMove : MonoBehaviour
     public bool _isStarted = false;
     public bool _isEnded = false;
     public bool _enemyWin = false;
+    public bool _canClick = false;
 
     //----------------------------------------------------//
 
     [Header("Statistique")]
 
     public float clickPerSecond, _timer = 1;
-    public float _maxValue = 0.6f;
+    public float _maxValue = 0.9f;
     public float _ratio = 10;
 
+    //----------------------------------------------------//
+
+    [Header("Start Game")]
+
+    public Animator _animator = null;
 
     //----------------------------------------------------//
 
@@ -34,25 +40,38 @@ public class AdamMove : MonoBehaviour
     {
         _clickCounter.maxValue = 1;
         _clickCounter.value = .01f;
+        _canClick = false;
     }
     private void Update()
     {
         if(_isStarted == true)
         {
             _timer += Time.deltaTime;
-            if(_clickCounter.value < 1) _clickCounter.value = Mathf.InverseLerp(0, 14, clickPerSecond / _timer);
+            if(_clickCounter.value < 1) _clickCounter.value = Mathf.InverseLerp(0, 9, clickPerSecond / _timer);
         }
 
         SnailEndGame();
     }
 
+    private void FixedUpdate()
+    {
+        _player.position += new Vector3(_clickCounter.value / (10 / (_ratio / 10)), 0, 0);
+    }
+
     public void MovePlayer()
     {
-        _player.position += new Vector3(_clickCounter.value / (10/(_ratio/10)), 0, 0);
+        if (_isStarted == true)
+        {
+            if (_canClick == true) clickPerSecond++;
+        }
+        else
+        {
+            _isStarted = true;
+            StartCoroutine(StartGame());
+            Debug.Log("Test");
+        }
 
-        if (_isStarted == true) clickPerSecond++;
 
-        _isStarted = true;
     }
 
     public void SnailEndGame()
@@ -60,12 +79,19 @@ public class AdamMove : MonoBehaviour
         if(_clickCounter.value > _maxValue || _enemyWin == true)
         {
             Debug.Log("Game is losed");
-            SceneManager.LoadScene(1);
+            _canClick = false;
         }
 
         if(_isEnded == true)
         {
             // Take Next Card
         }
+    }
+
+    IEnumerator StartGame()
+    {
+        _animator.SetTrigger("Start");
+        yield return new WaitForSeconds(3.2f);
+        _canClick = true;
     }
 }
