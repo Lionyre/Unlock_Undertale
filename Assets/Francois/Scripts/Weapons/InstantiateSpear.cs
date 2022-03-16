7db86e40883b1bae6f4a390931dc5629463b7bab
Lionyre <95347628+Lionyre@users.noreply.github.com>
2022-03-16 09:18:39 +0100

    TimingDesLances
    Le timing des lances était trop court, c'est ce qu'on nomme se faire "marabouter" vu la vitesse de spawn

--- Assets/Francois/Scripts/Weapons/InstantiateSpear.cs
+++ Assets/Francois/Scripts/Weapons/InstantiateSpear.cs
@@ -6,12 +6,13 @@ public class InstantiateSpear : MonoBehaviour
 {
     public GameObject SpearPrefab;
     private float TimerSpawn = 5f;
+    [SerializeField] private GameObject Player;
 
     // Update is called once per frame
     void FixedUpdate()
     {
         TimerSpawn -= Time.fixedDeltaTime;
-        if(TimerSpawn <= 0)
+        if(TimerSpawn <= 0 && Player.GetComponent<Health>().CurrentHealth > 0)
         {
             StartCoroutine("SpawnLance");
         }
@@ -21,9 +22,9 @@ public class InstantiateSpear : MonoBehaviour
     {
         TimerSpawn = 5f;
         Instantiate(SpearPrefab, new Vector3(Random.Range(-3.5f,3.5f), Random.Range(1.5f,3.85f), 0f), Quaternion.identity);
-         yield return new WaitForSeconds(0.4f);
+         yield return new WaitForSeconds(0.6f);
         Instantiate(SpearPrefab, new Vector3(Random.Range(-3.5f,3.5f), Random.Range(1.5f,3.85f), 0f), Quaternion.identity);
-        yield return new WaitForSeconds(0.4f);
+        yield return new WaitForSeconds(0.6f);
         Instantiate(SpearPrefab, new Vector3(Random.Range(-3.5f,3.5f), Random.Range(1.5f,3.85f), 0f), Quaternion.identity);
     }
 }
