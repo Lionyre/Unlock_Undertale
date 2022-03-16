eb72f1e3fdd4183c94e93f49efe6f793ff94ee54
Poneyyy666 <87376902+Poneyyy666@users.noreply.github.com>
2022-03-16 10:04:39 +0100

    C'est la fin de le jeu
    le jeu est terminé :'(

--- Assets/Script/Slidin.cs
+++ Assets/Script/Slidin.cs
@@ -8,39 +8,39 @@ public class Slidin : MonoBehaviour
     private Animator direction;
 
     private void OnTriggerEnter2D(Collider2D other) {
-        Debug.Log("sdfsdfd");
         if (other.gameObject.layer == 0 && other.gameObject.name == "Player")
         {
+            other.gameObject.GetComponent<Move>().avancement -= 1;
             player.orange = false;
-            player.stopAll = true;
+            // player.stopAll = true;
         }
     }
-    private void OnTriggerStay2D(Collider2D other) {
-        if (other.gameObject.layer == 0 && other.gameObject.name == "Player")
-        {
-            direction = other.gameObject.GetComponent<Animator>();
-            if(direction.GetCurrentAnimatorStateInfo(0).IsName("up"))
-            {
-                player.transform.position += transform.up * player.speed *Time.deltaTime;
-            }
-            if(direction.GetCurrentAnimatorStateInfo(0).IsName("down"))
-            {
-                player.transform.position -= transform.up * player.speed *Time.deltaTime;
-            }
-            if(direction.GetCurrentAnimatorStateInfo(0).IsName("right"))
-            {
-                player.transform.position += transform.right * player.speed *Time.deltaTime;
-            }
-            if(direction.GetCurrentAnimatorStateInfo(0).IsName("left"))
-            {
-                player.transform.position -= transform.right * player.speed *Time.deltaTime;
-            }
-        }
-    }
-    private void OnTriggerExit2D(Collider2D other) {
-        if (other.gameObject.layer == 0 && other.gameObject.name == "Player")
-        {
-            player.stopAll = false;
-        }
-    }
+    // private void OnTriggerStay2D(Collider2D other) {
+    //     if (other.gameObject.layer == 0 && other.gameObject.name == "Player")
+    //     {
+    //         direction = other.gameObject.GetComponent<Animator>();
+    //         if(direction.GetCurrentAnimatorStateInfo(0).IsName("up"))
+    //         {
+    //             player.transform.position += transform.up * player.speed *Time.deltaTime;
+    //         }
+    //         if(direction.GetCurrentAnimatorStateInfo(0).IsName("down"))
+    //         {
+    //             player.transform.position -= transform.up * player.speed *Time.deltaTime;
+    //         }
+    //         if(direction.GetCurrentAnimatorStateInfo(0).IsName("right"))
+    //         {
+    //             player.transform.position += transform.right * player.speed *Time.deltaTime;
+    //         }
+    //         if(direction.GetCurrentAnimatorStateInfo(0).IsName("left"))
+    //         {
+    //             player.transform.position -= transform.right * player.speed *Time.deltaTime;
+    //         }
+    //     }
+    // }
+    // private void OnTriggerExit2D(Collider2D other) {
+    //     if (other.gameObject.layer == 0 && other.gameObject.name == "Player")
+    //     {
+    //         player.stopAll = false;
+    //     }
+    // }
 }
