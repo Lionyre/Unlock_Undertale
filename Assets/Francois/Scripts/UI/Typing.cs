f4d6cf93bfd961a942a6599fe882652b7b946e98
Poneyyy666 <87376902+Poneyyy666@users.noreply.github.com>
2022-03-25 16:59:54 +0100

    Typing

---
+++ Assets/Francois/Scripts/UI/Typing.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Typing : MonoBehaviour
+{
+    private string number = "";
+    public TextMeshPro Input;
+
+    public void Zero(){
+        number += "0";
+    }
+    public void Un(){
+        number += "1";
+    }
+    public void Deux(){
+        number += "2";
+    }
+    public void Trois(){
+        number += "3";
+    }
+    public void Quatre(){
+        number += "4";
+    }
+    public void Cinq(){
+        number += "5";
+    }
+    public void Six(){
+        number += "6";
+    }
+    public void Sept(){
+        number += "7";
+    }
+    public void Huit(){
+        number += "8";
+    }
+    public void Neuf(){
+        number += "9";
+    }
+    public void Back(){
+        number = number.Substring(0, number.Length - 1);
+    }
+    public void C(){
+        number = "";
+    }
+    private void Update() {
+        Input.text = number;
+    }
+}
